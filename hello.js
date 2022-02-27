ec1e2af308148c7466585a5a2845b5e8725443ff
lizcastroprogrammer <elizabethjcastro42@gmail.com>
2022-02-27 10:22:16 -0600

    adds greet function and tests

--- hello.js
+++ hello.js
@@ -1,3 +1,15 @@
-function hello() {
-  return "Hello World!";
-}
+const greet = (name) => {
+  if(name === null) {
+    return 'Hello there!';
+  } else if(Array.isArray(name)) {
+    let string = 'Hello';
+    name.forEach(element => string += ',' + element);
+    return string;
+  } else if(name === name.toUpperCase()) {
+    return `HELLO ${name}!`;
+  } else {
+    return `Hello, ${name}`;
+  }
+};
+
+module.exports = greet;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,19 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+const greet = require('./hello');
 
-test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+describe('test greet()', () => {
+  it('should return a string with a name', () => {
+    expect(greet('Elizabeth')).toBe('Hello, Elizabeth');
+  });
+  it('should return a string with null name', () => {
+    expect(greet(null)).toBe('Hello there!');
+  });
+  it('should return an uppercase string', () => {
+    expect(greet('JOSE')).toBe('HELLO JOSE!');
+  });
+  it('should return a string with two names', () => {
+    expect(greet(['Jose', 'Pep'])).toBe('Hello,Jose,Pep');
+  });
+  it('should return a string with multiple names', () => {
+    expect(greet(['Alex', 'Arsene', 'Jose', 'Zidane'])).toBe('Hello,Alex,Arsene,Jose,Zidane');
+  });
 });
